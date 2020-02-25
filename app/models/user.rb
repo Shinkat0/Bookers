64d2f73e8b085c5f20ec7f625421e9d9e08fbea0
Shinkat0 <cntaro1234@gmail.com>
2020-02-25 08:49:33 +0000

    [add] username to signin,signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+ validates :username,uniqueness: true
+ devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :trackable, :validatable
+
+ def email_required?
+   false
+ end
+ def email_changed?
+   false
+ end
 end
